c0df9418018f500d3316b01823821837c6b780c5
Daniel Mihalca <daniel.mihalca@outlook.com>
2024-03-07 21:52:13 +0200

    Define only EE_HANDS for split sofle

--- keyboards/sofle/keymaps/rgb_default/config.h
+++ keyboards/sofle/keymaps/rgb_default/config.h
@@ -23,7 +23,7 @@
 ///https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
 
 #define SPLIT_USB_DETECT
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
 #define EE_HANDS
 
